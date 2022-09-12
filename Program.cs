e912cfaa26a3708238cccc7c2859fc56b3718f39
Korisnik <Korisnik@DESKTOP-Q60U0Q5>
2022-09-12 11:56:29 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    internal class Program
+    {
+        
+
+        static void Main(string[] args)
+        {
+            int a, b;
+            double c, d;
+            a = 10;
+            b = 7;
+            c = 12.5;
+            d = 16.7;
+            Console.WriteLine("Prosjecna vrijednost je: " + ((a + b + c + d) / 4));
+            Console.ReadKey();
+        }
+    }
+}
